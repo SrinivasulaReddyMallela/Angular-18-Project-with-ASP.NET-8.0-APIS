52d05e398c2c7dc14cfbdd29889bec2ca40e84aa
srinivasulareddymallela <srinivasulareddy.in@gmail.com>
2024-10-04 14:29:30 +0530

    added react project to GIT

---
+++ gym-web-react/gymweb-react-app/src/app/hooks/useNotification.js
@@ -0,0 +1,5 @@
+import { useContext } from "react";
+import NotificationContext from "app/contexts/NotificationContext";
+
+const useNotification = () => useContext(NotificationContext);
+export default useNotification;
